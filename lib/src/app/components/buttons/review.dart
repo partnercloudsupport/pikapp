3e3ab01676cbefae0bb0cc117315a086a9a4c82d
Lorenzo Pieri <me@lrnz09.me>
2018-06-16 13:14:13 +0200

    Add review button with showDialog on press

--- lib/src/app/components/buttons/review.dart
+++ lib/src/app/components/buttons/review.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
@@ -17,13 +19,46 @@ class ReviewIconButton extends StatelessWidget {
     }
   }
 
+  AlertDialog _buildDialog(BuildContext context) {
+    AppLocalizations localizations = AppLocalizations.of(context);
+    String dialog_title = localizations.translate('review_dialog_title');
+    String dialog_content = localizations.translate('review_dialog_content');
+    String text_later = localizations.translate('text_later').toUpperCase();
+    String text_sure = localizations.translate('text_sure').toUpperCase();
+
+    return AlertDialog(
+      title: Text(dialog_title),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[Text(dialog_content)],
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text(text_later),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text(text_sure),
+          onPressed: () {
+            Navigator.of(context).pop();
+            _onPressed();
+          },
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     String tooltip = AppLocalizations.of(context).translate('review_tooltip');
 
     return IconButton(
       icon: Icon(Icons.favorite),
-      onPressed: _onPressed,
+      onPressed: () =>
+          showDialog<Null>(context: context, builder: _buildDialog),
       tooltip: tooltip,
     );
   }
